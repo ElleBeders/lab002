6ee4cb6ef79d1dfaf05e2dd105eb0b7aa87ec53f
ElleBeders <malefisenta135@gmail.com>
2026-03-06 09:59:31 +0500

    Add files via upload

---
+++ 01/ConsoleApp2/Program.fs
@@ -0,0 +1,38 @@
+﻿open System
+
+// Функция для считывания римских цифр от пользователя
+let rec readRoman acc =
+    let input = Console.ReadLine()
+    if input = "000" then
+        List.rev acc  // Разворачиваем список, чтобы сохранить порядок ввода
+    else
+        readRoman (input :: acc)  // Добавляем введённую цифру в аккумулятор
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите римские цифры (I, II, III, IV, V, VI, VII, VIII, IX):"
+    printfn "(000 — конец ввода)"
+    
+    // Считываем список римских цифр от пользователя
+    let romanNumerals = readRoman []
+    
+    // Преобразуем список римских цифр в десятичные числа с помощью List.map
+    let decimalNumbers =
+        List.map (fun roman ->
+            match roman with
+            | "I" -> 1
+            | "II" -> 2
+            | "III" -> 3
+            | "IV" -> 4
+            | "V" -> 5
+            | "VI" -> 6
+            | "VII" -> 7
+            | "VIII" -> 8
+            | "IX" -> 9
+            | _ -> 0  // На случай неизвестной римской цифры
+        ) romanNumerals
+    
+    // Выводим результаты
+    printfn "Римские цифры: %A" romanNumerals
+    printfn "Десятичные числа: %A" decimalNumbers
+    0
---
+++ 02/ConsoleApp2/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+// Сисок пользователя
+let rec readR acc =
+    let ch = Console.ReadLine()
+    if ch = "000" then
+        List.rev acc
+    else
+        readR (int(ch) :: acc) 
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите цифры (по одной):"
+    printfn "(000 — конец ввода)"
+    
+    let nume = readR []
+    
+    let ch =
+        List.fold (fun acc x ->
+            if x % 2 = 0 then  
+                acc * 10 + x  
+                // + el (сдвигая разряды)
+            else
+                acc 
+        ) 0 nume 
+    
+    printfn "Исходные цифры: %A" nume
+    printfn "Число из чётных цифр: %i" ch
+    0
+
